41cff3feff6c8037f0c61e70d4c3649278939eac
cterrasid <clarette.terrasi@gmail.com>
2019-04-22 20:01:42 +0200

    Create loop for into fetch

--- _src/assets/js/main.js
+++ _src/assets/js/main.js
@@ -2,30 +2,42 @@
 
 const inputEl = document.querySelector('.searcher-input');
 const buttonEl = document.querySelector('.searcher-btn');
-const unListEl = document.querySelector('.result-list');
 const urlSearch = 'http://api.tvmaze.com/search/shows?q=';
-//const urlBase = 'http://www.tvmaze.com/api#search';
 
 buttonEl.addEventListener('click', handleButtonClick);
 
 function handleButtonClick() {
-    //Al hacer click el valor del input se incorpora a la url
-    const inputValue = inputEl.value;
-    const urlResult = urlSearch + inputValue;
+  //Al hacer click el valor del input se incorpora a la url
+  const inputValue = inputEl.value;
+  const urlResult = urlSearch + inputValue;
 
-    fetch(urlResult)
-        .then(response => response.json())
-        .then(show => {
-            //muestrame los resultados
-            //const infoResult = data.show;
-            //con cada iteracion me buscas la imagen y el nombre del show
-            for (let i = 0; i < show.length; i++) {
-                const imageShow = show[i].image;
-                const nameShow = show[i].name;
-                
-                
-            }
+  fetch(urlResult)
+    .then(response => response.json())
+    .then(data => {
+        //bucle horrible
+      for (let index = 0; index < data.length; index++) {
+        // const infoResult = data[index];
+        let imgUrl = data[index].show.image.medium;
+        let title = data[index].show.name;
+        let ulListEl = document.querySelector('.result-list');
+        //creo un li en cada iteracion
+        const liEl = document.createElement('li');
+        //creo un img en cada iteracion
+        const imgEl = document.createElement('img');
+        //creo un titulo en cada iteracion
+        const titleEl = document.createElement('h3');
+        //meto el img en el li
+        liEl.appendChild(imgEl);
+        //meto el titulo en el li
+        liEl.appendChild(titleEl);
+        //meto el li en ul
+        ulListEl.appendChild(liEl);
 
-            });
 
-        }
+
+      }
+
+
+    });
+
+}
