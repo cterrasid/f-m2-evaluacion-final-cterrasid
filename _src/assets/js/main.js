80c98ba121e1079fa472edecc4b854e6219f4db5
cterrasid <clarette.terrasi@gmail.com>
2019-04-22 22:10:49 +0200

    Add alternative image option

--- _src/assets/js/main.js
+++ _src/assets/js/main.js
@@ -8,7 +8,7 @@ const urlSearch = 'http://api.tvmaze.com/search/shows?q=';
 buttonEl.addEventListener('click', handleButtonClick);
 
 function handleButtonClick() {
-    //Establezco una condicion de 'vacio'
+    //Establezco una condicion de 'vacio' inicial
     ulListEl.innerHTML = '';
   //Al hacer click el valor del input se incorpora a la url
   const inputValue = inputEl.value;
@@ -17,17 +17,22 @@ function handleButtonClick() {
   fetch(urlResult)
     .then(response => response.json())
     .then(data => {
-        //bucle horrible
+    //bucle horrible
       for (let index = 0; index < data.length; index++) {
         // const infoResult = data[index];
         let imgUrl = data[index].show.image.medium;
         let title = data[index].show.name;
+        const imgUrlAlt = `https://via.placeholder.com/210x295/ffffff/666666/?text=${title}`;
         //creo un li en cada iteracion
         const liEl = document.createElement('li');
         //creo un img en cada iteracion
         const imgEl = document.createElement('img');
         //añado el src y la url de la imagen
-        imgEl.setAttribute('src', imgUrl);
+        if (imgUrl) {
+            imgEl.setAttribute('src', imgUrl);
+        } else {
+            imgEl.setAttribute('src', imgUrlAlt);            
+        }
         //creo un titulo en cada iteracion
         const titleEl = document.createElement('h3');
         //añado contenido dentro del titulo
@@ -39,11 +44,18 @@ function handleButtonClick() {
         //meto el li en ul
         ulListEl.appendChild(liEl);
 
-
-
       }
 
 
     });
 
 }
+
+//FAVORITE SHOWS
+// Una vez aparecen los resultados de búsqueda, podremos indicar cuáles son nuestras series
+// favoritas. Para ello, al hacer clic sobre un resultado el color de fondo y el de fuente se
+// intercambian.
+// Además, debes crear un listado (array) con las series favoritas que almacenamos en una variable.
+// Este listado lo mostraremos en la parte izquierda de la pantalla, debajo del formulario de búqueda.
+// Para terminar, si volvemos a realizar una nueva búsqueda, los favoritos se irán acumulando en
+// nuestra lista.
